692187b0f6d89d8a3ef61e65597c3c0368c686de
Pink <649518322@qq.com>
2020-02-17 00:29:31 +0800

    括号合法性

--- src/stack/StackBasedOnLinkedList.java
+++ src/stack/StackBasedOnLinkedList.java
@@ -1,18 +1,21 @@
 package stack;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * 链式栈
  *
  * @author Pink
  *
- * @version $Id: StackBasedOnLinkedList, v 0.1 2020年02月16日 01:59:58  Pink Exp $
+ * @version $Id: StackBasedOnLinkedList, v 0.1 2020年02月16日 01:59:58 Pink Exp $
  */
 public class StackBasedOnLinkedList {
 
 	private Node top;
 
 	// 入栈
-	private void push(int value) {
+	private void push(String value) {
 		Node newNode = new Node(value, null);
 		if (top == null) {
 			top = newNode;
@@ -23,13 +26,50 @@ public class StackBasedOnLinkedList {
 	}
 
 	// 出栈
-	private int pop() {
+	private String pop() {
 		if (top == null) {
-			return -1;
+			return null;
 		}
-		int value = top.value;
+		String value = top.value;
 		top = top.next;
 		return value;
+	}
+
+	/***
+	 * {[]()[{}]}或[{()}([])]等都为合法格式，而{[}()]或[({)]为不合法的格式。那我现在给你一个包含三种括号的表达式字符串，如何检查它是否合法呢？
+	 */
+	public boolean checkString(List<String> values) {
+
+		for (String value : values) {
+			if (value.equals("{") || value.equals("[") || value.equals("(")) {
+				push(value);
+			} else {
+				String left = pop();
+				if (left == null) {
+					return false;
+				}
+				if (value.equals("}")) {
+					if (left.equals("{")) {
+						continue;
+					} else {
+						return false;
+					}
+				} else if (value.equals(")")) {
+					if (left.equals("(")) {
+						continue;
+					} else {
+						return false;
+					}
+				} else {
+					if (left.equals("[")) {
+						continue;
+					} else {
+						return false;
+					}
+				}
+			}
+		}
+		return true;
 	}
 
 	private void printAll() {
@@ -43,20 +83,20 @@ public class StackBasedOnLinkedList {
 
 	private class Node {
 
-		private int value;
+		private String value;
 
 		private Node next;
 
-		public Node(int value, Node next) {
+		public Node(String value, Node next) {
 			this.value = value;
 			this.next = next;
 		}
 
-		public int getValue() {
+		public String getValue() {
 			return value;
 		}
 
-		public void setValue(int value) {
+		public void setValue(String value) {
 			this.value = value;
 		}
 
@@ -71,14 +111,26 @@ public class StackBasedOnLinkedList {
 
 	public static void main(String[] args) {
 		StackBasedOnLinkedList list = new StackBasedOnLinkedList();
-		int i = 0;
-		while (i < 5) {
-			list.push(i);
-			list.printAll();
-			i++;
-		}
+//
+//		list.push("1");
+//		list.push("2");
+//		list.push("3");
+//		list.push("4");
+//		list.push("5");
+//		list.printAll();
+//
+//		list.pop();
+//		list.printAll();
 
-		list.pop();
-		list.printAll();
+		List<String> values = new ArrayList<>();
+		values.add("{");
+		values.add("[");
+		values.add("]");
+		values.add("[");
+		values.add("{");
+		values.add("}");
+		values.add("]");
+		values.add("}");
+		System.out.println(list.checkString(values));
 	}
 }
